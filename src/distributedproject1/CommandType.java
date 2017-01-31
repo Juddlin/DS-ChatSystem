7a563f1c7d70d7f436420d97e3fcffb8895c3de7
james <james@192.168.0.19>
2017-01-31 14:18:17 -0500

    Tested CommandParser and made a few minor fixes, all classes seem to be working with the parser

---
+++ src/distributedproject1/CommandType.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributedproject1;
+
+/**
+ *
+ * @author james
+ */
+public enum CommandType {
+    CHAT_MESSAGE, CHAT_REQUEST, CLIENT_UPDATE_MESSAGE, 
+    CONNECT_COMMAND, CONNECT_RESPONSE, JOIN_CHATROOM_COMMAND,
+    JOIN_CHATROOM_RESPONSE
+    
+    
+    
+}
